9bead0fe1a436fd76052128e0ea30f706b6d4aa6
Ankita Singh <86732206+ankita0102@users.noreply.github.com>
2023-04-10 11:42:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static('public')); 
 app.use(express.json())
 
-app.get('/', (req, res)=>{
+app.get('/public/', (req, res)=>{
     res.sendFile(__dirname + '/public/index.html') 
 }) 
 
